65511b8366faa6c6bd8ab0661673fc84864438b9
mh <mh@immerda.ch>
2012-02-09 16:05:01 +0100

    file_line is now also ensurable

--- manifests/rclocal/debian.pp
+++ manifests/rclocal/debian.pp
@@ -1,7 +1,7 @@
 class ibp::rclocal::debian {
-  line{'remove_exit_0_from_rc_local_to_enabe_it':
+  file_line{'remove_exit_0_from_rc_local_to_enabe_it':
     line => 'exit 0',
-    file => '/etc/rc.local',
+    path => '/etc/rc.local',
     ensure => 'absent',
   }
 }
