e569a47f83fc23cd1d47add927bc2caefe6dfc9e
mh <mh@immerda.ch>
2014-07-09 22:59:41 +0200

    we don't have any lenny systems anymore and grub2 systems are getting mainstream

--- manifests/panics/linux.pp
+++ manifests/panics/linux.pp
@@ -1,42 +1,37 @@
 class ibp::panics::linux {
   exec{"grub_panics": }
   # grub 2.0 systems
-  if ($::operatingsystem == 'Debian') and ($::lsbdistcodename != 'lenny') {
-    Exec["grub_panics"]{
-      command => "sed -i 's@^\\(GRUB_CMDLINE_LINUX=\".*\\)\"@\\1 panic=30\"@g' /etc/default/grub",
-      unless => "grep -qE '^GRUB_CMDLINE_LINUX=.*panic=30.*' /etc/default/grub",
-      notify => Exec['update_grub_2.0'],
-    }
-    exec{'update_grub_2.0':
-      command => 'update-grub',
-      refreshonly => true,
-    }
-  } else {
+  if ($::operatingsystem == 'Centos') and ($::operatingsystemmajrelease < 7) {
     $grubmenu_location = $::operatingsystem ? {
-      debian => "/boot/grub/menu.lst",
-      ubuntu => "/boot/grub/menu.lst",
+      debian  => "/boot/grub/menu.lst",
+      ubuntu  => "/boot/grub/menu.lst",
       default => "/boot/grub/grub.conf"
     }
     Exec["grub_panics"]{
       command => "sed -i 's@\\(.*vmlinuz.*\\)@\\1 panic=30@g' ${grubmenu_location}",
-      unless => "grep -q 'panic=30' ${grubmenu_location}",
+      unless  => "grep -q 'panic=30' ${grubmenu_location}",
+    }
+  } else {
+    Exec["grub_panics"]{
+      command => "sed -i 's@^\\(GRUB_CMDLINE_LINUX=\".*\\)\"@\\1 panic=30\"@g' /etc/default/grub",
+      unless  => "grep -qE '^GRUB_CMDLINE_LINUX=.*panic=30.*' /etc/default/grub",
+      notify  => Exec['update_grub_2.0'],
+    }
+    exec{'update_grub_2.0':
+      command     => 'update-grub',
+      refreshonly => true,
     }
   }
 
   sysctl::value{
     # set the reboot time for kernel panics
     'kernel.panic':
-        value => '30';
-  }
-  # these values aren't yet present on a debian lenny
-  if !(($::operatingsystem == 'Debian') and ($::lsbdistcodename == 'lenny')) {
-	  sysctl::value{
-	    # Panic if a hung task was found
-	    'kernel.hung_task_panic':
-	        value => 1;
-	    # Setup timeout for hung task to 300 seconds
-	    'kernel.hung_task_timeout_secs':
-	        value => 300;
-	  }
+      value => '30';
+    # Panic if a hung task was found
+    'kernel.hung_task_panic':
+      value => 1;
+    # Setup timeout for hung task to 300 seconds
+    'kernel.hung_task_timeout_secs':
+      value => 300;
   }
 }
