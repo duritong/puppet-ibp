e1394f92c99ac89d237001c923676eb1272f2eb9
mh <mh@immerda.ch>
2014-07-11 18:38:14 +0200

    updates for el7

--- manifests/panics/linux.pp
+++ manifests/panics/linux.pp
@@ -3,22 +3,9 @@ class ibp::panics::linux {
   exec{'grub_panics': }
   # grub 2.0 systems
   if ($::operatingsystem == 'Centos') and ($::operatingsystemmajrelease < 7) {
-    $grubmenu_location = $::operatingsystem ? {
-      debian  => '/boot/grub/menu.lst',
-      ubuntu  => '/boot/grub/menu.lst',
-      default => '/boot/grub/grub.conf'
-    }
     Exec['grub_panics']{
-      command => "sed -i 's@\\(.*vmlinuz.*\\)@\\1 panic=30@g' ${grubmenu_location}",
-      unless  => "grep -q 'panic=30' ${grubmenu_location}",
-    }
-    sysctl::value{
-      # Panic if a hung task was found
-      'kernel.hung_task_panic':
-        value => 1;
-      # Setup timeout for hung task to 300 seconds
-      'kernel.hung_task_timeout_secs':
-        value => 300;
+      command => 'sed -i \'s@\\(.*vmlinuz.*\\)@\\1 panic=30@g\' /boot/grub/grub.conf',
+      unless  => 'grep -q \'panic=30\' /boot/grub/grub.conf'
     }
   } else {
     Exec['grub_panics']{
@@ -26,8 +13,14 @@ class ibp::panics::linux {
       unless  => 'grep -qE \'^GRUB_CMDLINE_LINUX=.*panic=30.*\' /etc/default/grub',
       notify  => Exec['update_grub_2.0'],
     }
+
+    if ($::operatingsystem == 'Centos') and ($::operatingsystemmajrelease > 6) {
+      $grub_update_cmd = 'grub2-mkconfig -o /boot/grub2/grub.cfg'
+    } else {
+      $grub_update_cmd = 'update-grub'
+    }
     exec{'update_grub_2.0':
-      command     => 'update-grub',
+      command     => $grub_update_cmd,
       refreshonly => true,
     }
   }
@@ -36,5 +29,15 @@ class ibp::panics::linux {
     # set the reboot time for kernel panics
     'kernel.panic':
       value => '30';
+  }
+  if !($::operatingsystem == 'Centos' and $::operatingsystemmajrelease > 6) {
+    sysctl::value{
+      # Panic if a hung task was found
+      'kernel.hung_task_panic':
+        value => 1;
+      # Setup timeout for hung task to 300 seconds
+      'kernel.hung_task_timeout_secs':
+        value => 300;
+    }
   }
 }
