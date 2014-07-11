a957c9166dc8e8c68a82ddaf837244c6e3b8e74d
mh <mh@immerda.ch>
2014-07-11 18:27:57 +0200

    kernel hung task is gone on el7

--- manifests/panics/linux.pp
+++ manifests/panics/linux.pp
@@ -1,20 +1,29 @@
+# optimizations that we do on our linux installations
 class ibp::panics::linux {
-  exec{"grub_panics": }
+  exec{'grub_panics': }
   # grub 2.0 systems
   if ($::operatingsystem == 'Centos') and ($::operatingsystemmajrelease < 7) {
     $grubmenu_location = $::operatingsystem ? {
-      debian  => "/boot/grub/menu.lst",
-      ubuntu  => "/boot/grub/menu.lst",
-      default => "/boot/grub/grub.conf"
+      debian  => '/boot/grub/menu.lst',
+      ubuntu  => '/boot/grub/menu.lst',
+      default => '/boot/grub/grub.conf'
     }
-    Exec["grub_panics"]{
+    Exec['grub_panics']{
       command => "sed -i 's@\\(.*vmlinuz.*\\)@\\1 panic=30@g' ${grubmenu_location}",
       unless  => "grep -q 'panic=30' ${grubmenu_location}",
     }
+    sysctl::value{
+      # Panic if a hung task was found
+      'kernel.hung_task_panic':
+        value => 1;
+      # Setup timeout for hung task to 300 seconds
+      'kernel.hung_task_timeout_secs':
+        value => 300;
+    }
   } else {
-    Exec["grub_panics"]{
+    Exec['grub_panics']{
       command => "sed -i 's@^\\(GRUB_CMDLINE_LINUX=\".*\\)\"@\\1 panic=30\"@g' /etc/default/grub",
-      unless  => "grep -qE '^GRUB_CMDLINE_LINUX=.*panic=30.*' /etc/default/grub",
+      unless  => 'grep -qE \'^GRUB_CMDLINE_LINUX=.*panic=30.*\' /etc/default/grub',
       notify  => Exec['update_grub_2.0'],
     }
     exec{'update_grub_2.0':
@@ -27,11 +36,5 @@ class ibp::panics::linux {
     # set the reboot time for kernel panics
     'kernel.panic':
       value => '30';
-    # Panic if a hung task was found
-    'kernel.hung_task_panic':
-      value => 1;
-    # Setup timeout for hung task to 300 seconds
-    'kernel.hung_task_timeout_secs':
-      value => 300;
   }
 }
