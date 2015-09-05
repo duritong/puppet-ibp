d780eb6816516c4a9954a503cc37f53b91cb387b
o <o@immerda.ch>
2015-09-05 12:42:47 +0200

    add option to disable grub support

--- manifests/panics/linux.pp
+++ manifests/panics/linux.pp
@@ -1,27 +1,32 @@
 # optimizations that we do on our linux installations
-class ibp::panics::linux {
-  exec{'grub_panics': }
-  # grub 2.0 systems
-  if ($::operatingsystem == 'Centos') and ($::operatingsystemmajrelease < 7) {
-    Exec['grub_panics']{
-      command => 'sed -i \'s@\(.*vmlinuz.*\)@\1 panic=30@g\' /boot/grub/grub.conf',
-      unless  => 'grep -q \'panic=30\' /boot/grub/grub.conf'
-    }
-  } else {
-    Exec['grub_panics']{
-      command => "sed -i 's@^\\(GRUB_CMDLINE_LINUX=\".*\\)\"@\\1 panic=30\"@g' /etc/default/grub",
-      unless  => 'grep -qE \'^GRUB_CMDLINE_LINUX=.*panic=30.*\' /etc/default/grub',
-      notify  => Exec['update_grub_2.0'],
-    }
+class ibp::panics::linux(
+  $use_grub = true
+) {
 
-    if ($::operatingsystem == 'Centos') and ($::operatingsystemmajrelease > 6) {
-      $grub_update_cmd = 'grub2-mkconfig -o /boot/grub2/grub.cfg'
+  if ($use_grub) {
+    exec{'grub_panics': }
+    # grub 2.0 systems
+    if ($::operatingsystem == 'Centos') and ($::operatingsystemmajrelease < 7) {
+      Exec['grub_panics']{
+        command => 'sed -i \'s@\(.*vmlinuz.*\)@\1 panic=30@g\' /boot/grub/grub.conf',
+        unless  => 'grep -q \'panic=30\' /boot/grub/grub.conf'
+      }
     } else {
-      $grub_update_cmd = 'update-grub'
-    }
-    exec{'update_grub_2.0':
-      command     => $grub_update_cmd,
-      refreshonly => true,
+      Exec['grub_panics']{
+        command => "sed -i 's@^\\(GRUB_CMDLINE_LINUX=\".*\\)\"@\\1 panic=30\"@g' /etc/default/grub",
+        unless  => 'grep -qE \'^GRUB_CMDLINE_LINUX=.*panic=30.*\' /etc/default/grub',
+        notify  => Exec['update_grub_2.0'],
+      }
+
+      if ($::operatingsystem == 'Centos') and ($::operatingsystemmajrelease > 6) {
+        $grub_update_cmd = 'grub2-mkconfig -o /boot/grub2/grub.cfg'
+      } else {
+        $grub_update_cmd = 'update-grub'
+      }
+      exec{'update_grub_2.0':
+        command     => $grub_update_cmd,
+        refreshonly => true,
+      }
     }
   }
 
